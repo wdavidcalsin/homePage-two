3f345071179e72a90952f0b08552ceaaa95a19ae
David Calsin <david2c142@gmail.com>
2020-08-21 23:23:39 -0500

    Home Page v.1

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/perfil">Perfil</router-link>
     </div>
+    
     <router-view/>
   </div>
 </template>
---
+++ src/views/Perfil.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="david">
+      <h1>Mi nombre es Willian David</h1>
+  </div>
+</template>
+
+<script lang='ts'>
+import {Component , Vue, Prop} from 'vue-property-decorator'
+import { component } from 'vue/types/umd'
+
+
+export default class Perfil extends Vue{}
+</script>
